8f3232d1d9c79eeb90056b145628aa5937fd4399
steveyangrh <ryang@ualberta.ca>
2016-07-21 14:33:00 -0600

    some files from Princeton U

---
+++ ppca_yang.m
@@ -0,0 +1,12 @@
+function [] = ppca_yang (R0,k)
+% probabilistic PCA for dimensionality reduction
+% created on July 21, 2016
+% Ronghao Yang
+
+% R0 is the input matrix which contains missing values
+% k specifies the number of basis
+
+
+
+
+end
