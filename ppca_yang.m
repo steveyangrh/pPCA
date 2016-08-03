ffe870391e17b0a1362c8ba1f926d604d0a90f3d
Steve Yang <ryang@ualberta.ca>
2016-08-02 21:54:59 -0600

    new files

--- ppca_yang.m
+++ ppca_yang.m
@@ -6,15 +6,23 @@
 
 % Created on July 21, 2016
 % Ronghao Yang
-function [W,X] = ppca_yang(R0,k)
+function [W,u,sigma_square,R] = ppca_yang(R0,k)
 
 	[n,m] = size(R0);
-
+    R = R0;
+    
 	indexNotNan = find(~isnan(R0));
 	% indexNotNan has the indices of all non-nan values
 
 	W = rand(n,k);
 	% initialize W with a random matrix
+    
+    u = rand(n,1);
+    % initialize u with a random vector, u is mean
+    
+    sigma_square = rand;
+    % sigma is standard deviation
+    
 
 	minImprovement = 0.0001;
 	rmse = 10^40;
@@ -25,16 +33,42 @@ function [W,X] = ppca_yang(R0,k)
   	% the iterative step for minimizing the error
 
 	  	% Expectation step (E)
-	  	% generally, with missing values, minimize ||Wx*-y*||
-	  	%X = inv(W'* W)*W'*R0;
-	  	beta = W'*(W*W'+sigmaY^2.*eye());
-
+        SIG = W*W' + sigma_square.*eye(n);
+        for i=1:m
+            r = R0(:,i);
+            rNoNan = find(~isnan(r));
+            rNan = find(isnan(r));
+            r0 = r(rNoNan);
+            u0 = u(rNoNan);
+            uh = u(rNan);
+            SIG_OO = SIG (rNoNan,rNoNan);
+            SIG_HO = SIG (rNan,rNoNan);
+            r(rNan) = uh + SIG_HO*inv(SIG_OO)*(r0-u0);
+            R(:,i) = r;
+        end
+        
+        C = zeros(n,n);
 	  	% The minimization step (M)
-	  	%W = R0*X'*inv(X*X');
+        for i = 1:m
+            r = R(:,i);
+            C = C + (r-u)*(r-u)';
+        end
+        
+        C = C / m;
+        
+        u = sum(R,2);
+        
+        [U,D] = eigs(C,k);
+        
+        sigma_square = (trace(C)-trace(D))/(n-k);
+        
+        S = D- sigma_square;
+        
+        W = U*S;
 
 	    % Compute the error
 	    prevErr = rmse;
-	    %rmse = sqrt(mean(mean((R-W*H).^2)));
+	    rmse = computeRMSE(R0,R);
 
 	end
 
--- simpletest.m
+++ simpletest.m
@@ -2,10 +2,9 @@ clear
 clc
 close all
 
-for k = 6:1:9
-R0 = rand(200,k)*rand(k,200);
-R0 = nanGenerator({R0},80);
-R0 = R0{1};
+for k = 6:1:6
+R0_origin = rand(200,k)*rand(k,200);
+R0 = nanGenerator(R0_origin,80);
 subplot(2,2,k-5);
     for i = 1:10
         for j = 1:1:20
@@ -13,7 +12,7 @@ subplot(2,2,k-5);
 
             data_recon = (pinv(W)*pc)+repmat(data_mean,1,size(R0,2));
 
-            rmse = computeRMSE(R0,data_recon);
+            rmse = computeRMSE(R0_origin,data_recon);
 
             scatter(i,rmse);
             hold on;
